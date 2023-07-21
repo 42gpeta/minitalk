4f44083ae49b96a4ad54298cd38712beb651e9ba
Glo-dio <glodififa@gmail.com>
2023-07-21 15:30:34 +0200

    v 2.0 : affichage du PID dans server.c + envoi d'un signal a partir de client.c vers server.c

--- minitalk/src/client.c
+++ minitalk/src/client.c
@@ -6,13 +6,42 @@
 /*   By: gpeta <gpeta@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 19:30:36 by gpeta             #+#    #+#             */
-/*   Updated: 2023/07/17 17:05:51 by gpeta            ###   ########.fr       */
+/*   Updated: 2023/07/21 15:14:58 by gpeta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+// int	main(int ac, char **av)
+// {
+// 	pid_t pid;
+// 	struct sigaction sa;
+	
+// 	pid = ft_atoi(av[1]);
+	
+// 	sa.sa_handler = kill(pid, SIGTSTP);
+	
+// 	// sa.sa_handler = &handle_sigtstp;
+// 	sa.sa_flags  = SA_RESTART; // ! test
+// 	// sa.sa_flags  = SA_SIGINFO; // ! real
+
+// 	sigaction(SIGTSTP, &sa, NULL);
+
+// 	int x;
+// 	printf("Input number: ");
+// 	scanf("%d", &x);
+// 	printf("result %d * 5 = %d\n", x, x * 5);
+// 	return (0);
+// }
+
 int	main(int ac, char **av)
 {
+	int	pid;
+
+	pid = ft_atoi(av[1]);
+
+	printf("Envoi du signal SIGUSR1 au serveur (PID: %d)\n", pid);
+	kill(pid, SIGUSR1);
+
 	return (0);
 }
--- minitalk/src/server.c
+++ minitalk/src/server.c
@@ -6,7 +6,7 @@
 /*   By: gpeta <gpeta@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 19:30:41 by gpeta             #+#    #+#             */
-/*   Updated: 2023/07/17 18:01:30 by gpeta            ###   ########.fr       */
+/*   Updated: 2023/07/21 15:20:43 by gpeta            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,17 +31,18 @@ int main(int ac, char **av)
 	struct sigaction sa;
 	// sigaction sa;
 	
-	ft_get_pid();
 	sa.sa_handler = &handle_sigtstp;
-	sa.sa_flags  = SA_RESTART; // ! test
+	sigemptyset(&sa.sa_mask); // initialise tous les masques a 0
+	sa.sa_flags  = 0; // ! test
 	// sa.sa_flags  = SA_SIGINFO; // ! real
 
-	sigaction(SIGTSTP, &sa, NULL);
+	sigaction(SIGUSR1, &sa, NULL);
+	ft_get_pid();
 
-	int x;
-	printf("Input number: ");
-	scanf("%d", &x);
-	printf("result %d * 5 = %d\n", x, x * 5);
+	while (1)
+	{
+		sleep(1);
+	}
 
 	return (0);
 }
